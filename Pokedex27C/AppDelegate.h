a9b72fa8e1d9248fa2e8fba73995ab3078f1da18
Drew Seeholzer <drews21f7@gmail.com>
2019-07-02 09:57:07 -0600

    Initial commit

---
+++ Pokedex27C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex27C
+//
+//  Created by Drew Seeholzer on 7/2/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
